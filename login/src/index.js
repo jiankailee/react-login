075564a7d0632b165d1fc61ef6418ea4820e87e5
Jian Kai <lee_jk1996@hotmail.com>
2018-09-04 13:52:11 -0500

    create login

--- login/src/index.js
+++ login/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import login from './login'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
